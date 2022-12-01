8ac40dae24f8d6e97aefb13f473a72a2dc0cca11
Pedro Fonseca <fonseca.pmvasco@gmail.com>
2022-12-01 16:07:36 +0000

    Added 1st day solution

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,17 +1,50 @@
 import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
-  public solveFirst(): string {
-    return 'day 1 solution 1';
+  private getElfFoods(): string[] {
+    return `${this.input}\n`.split('\n');
   }
+
+  public solveFirst(): string {
+    const elfFoods = this.getElfFoods().reduce((agg, next) => {
+    const foodCalories = next.trim();
+    if (foodCalories.length === 0) 
+    {
+      return {
+        currentElfCalorieCount: 0,
+        maxCalorieCount: agg.maxCalorieCount < agg.currentElfCalorieCount ? agg.currentElfCalorieCount : agg.maxCalorieCount,
+      }
+    } 
+
+    agg.currentElfCalorieCount += Number(foodCalories);
+    return agg;
+    }, { currentElfCalorieCount: 0, maxCalorieCount: 0});
+    
+    return `${elfFoods.maxCalorieCount}`;
+  }
+
   public solveSecond(): string {
-    return 'day 1 solution 2';
+    const elfFoods = this.getElfFoods().reduce((agg, next) => {
+      const foodCalories = next.trim();
+      if (foodCalories.length === 0) 
+      {
+        return {
+          currentElfCalorieCount: 0,
+          max3CalorieCount: [...agg.max3CalorieCount, agg.currentElfCalorieCount].sort((a, b) => b - a).slice(0, 3)
+        }
+      } 
+  
+      agg.currentElfCalorieCount += Number(foodCalories);
+      return agg;
+      }, { currentElfCalorieCount: 0, max3CalorieCount: [0]});
+      
+      return `${elfFoods.max3CalorieCount.reduce((agg, calories) => agg+calories, 0)}`;
   }
 
   public getFirstExpectedResult(): string {
-    return 'day 1 solution 1';
+    return '24000';
   }
   public getSecondExpectedResult(): string {
-    return 'day 1 solution 2';
+    return '45000';
   }
 }
